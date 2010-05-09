a2944f447fd06ba174a54511e573657dce7e9f19
takahashim <maki@rubycolor.org>
2010-05-10 01:37:15 +0900

    add latex converter

---
+++ lib/hparser/latex.rb
@@ -0,0 +1,243 @@
+# Author:: Masayoshi Tajahashi (takahashimm@gmail.com)
+# Copyright:: Copyright (c) 2010 Masayoshi Takahashi
+# License::   Distributes under the same terms as Ruby
+# This file define +to_latex+. +to_latex+ is convert hatena format to LaTeX.
+#
+
+module HParser
+  # This module provide +to_latex+ method.
+  # This method is intended to convert hatena format to LaTeX format.
+  #
+  # For example:
+  #  Hatena::Parser.parse('*foo').to_latex # -> \section{foo}
+  #  Hatena::Parser.parse('>|bar|<').to_latex # -> \begin{verbatim} <\n> bar <\n> \end{verbatim}
+  #
+  # Unlike html, LaTeX phrase cannot be separated with tag and conent.
+  # so common methods, such like tag_name and html_content, are not always used.
+  # Only to_latex method is commonly impletemted.
+  # If content is +Arary+,each elements convert to LaTeX by
+  # +to_latex+. Otherwise,using as it self.
+  #
+  module Latex
+    def to_latex
+      content = latex_content
+      if content.class == Array then
+        content = content.map{|x| x.to_latex}.join
+      end
+      content
+    end
+  end
+
+  module Block
+    class Head
+      include Latex
+
+      def to_latex
+        content = super
+        headers = [
+                   nil,
+                   "section",
+                   "subsection",
+                   "subsubsection",
+                   "paragraph",
+                   "subparagraph",
+                   "textbf"
+                  ]
+        level = @@head_level + self.level - 1
+        "\\#{headers[level]}{#{content}}\n\n"
+      end
+
+      @@head_level = 1
+      def self.head_level=(l)
+        @@head_level = l
+      end
+      def self.head_level
+        @@head_level
+      end
+
+      alias_method :latex_content,:content
+    end
+
+    class P
+      include Latex
+
+      def to_latex
+        content = super
+        content += "\n\n"
+        content
+      end
+
+      alias_method :latex_content, :content
+    end
+
+    class Empty
+      def to_latex() "\n\n" end
+    end
+
+    class Pre
+      include Latex
+
+      def to_latex
+        content = super
+        %Q[\\begin{verbatim}\n#{content}\n\\end{verbatim}\n]
+      end
+
+      alias_method :latex_content, :content
+    end
+
+    class SuperPre
+      include Latex
+
+      def to_latex
+        content = latex_content  ## not 'super'
+        %Q[\\begin{verbatim}\n#{content}\n\\end{verbatim}\n]
+      end
+
+      alias_method :latex_content,:content
+    end
+
+    class Quote
+      include Latex
+
+      def to_latex
+        content = super
+        %Q[\\begin{quotation}\n#{content}\n\\end{quotation}\n]
+      end
+
+      alias_method :latex_content,:content
+    end
+
+    class Table
+      def to_latex
+        row_size = self.row_size
+
+        output  = "\\begin{table}\n"
+        output << "  \\centering\n"
+        output << "  \\begin{tabular}{ #{"l " * row_size }}\n"
+        self.map_row do |row|
+          output << "    #{row.map{|cell| cell.to_latex }.join(" & ")} \\\\\n"
+        end
+        output << "  \\end{tabular}\n"
+        output << "\\end{table}\n"
+        output
+      end
+
+      def row_size
+        self.map_row do |tr|
+          return tr.size
+        end
+      end
+    end
+
+    class Dl
+      include Latex
+
+      class Item
+        def to_latex
+          dt = self.title.map{|x| x.to_latex}.join
+          dd = self.description.map{|x| x.to_latex}.join
+
+          %Q(\\item[#{dt}] \\quad \\\\\n#{dd}\n)
+        end
+      end
+
+      def to_latex
+        content = super
+        %Q[\\begin{description}\n#{content}\n\\end{description}\n]
+      end
+
+      def latex_content
+        @items
+      end
+    end
+
+    class TableHeader
+      include Latex
+
+      def to_latex
+        content = super
+        content
+      end
+
+      alias_method :latex_content,:content
+    end
+
+    class TableCell
+      include Latex
+
+      def to_latex
+        content = super
+        content
+      end
+
+      alias_method :latex_content,:content
+    end
+
+    class UnorderList
+      include Latex
+
+      def to_latex
+        content = super
+        %Q[\\begin{itemize}\n#{items.map{|i| i.to_latex }.join("\n")}\n\\end{itemize}\n]
+      end
+      alias_method :latex_content,:items
+    end
+
+    class OrderList
+      include Latex
+
+      def to_latex
+        content = super
+        %Q[\\begin{enumerate}\n#{items.map{|i| i.to_latex }.join("\n")}\n\\end{enumerate}\n]
+      end
+
+      alias_method :latex_content,:items
+    end
+
+
+    class ListItem
+      include Latex
+
+      def to_latex
+        content = super
+        "  \\item #{content}\n"
+      end
+
+      alias_method :latex_content,:content
+    end
+
+    class RAW
+      def to_latex
+        @content.map {|i| i.to_latex }.join
+      end
+    end
+  end
+
+  module Inline
+    class Text
+      def to_latex
+        self.text
+      end
+    end
+
+    class Url
+      def to_latex
+        "\\href{#{self.url}/}{#{self.url}}"
+      end
+    end
+
+    class HatenaId
+      def to_latex
+        "\\href{http://d.hatena.ne.jp/#{self.name}/}{id:#{self.name}}"
+      end
+    end
+
+    class Comment
+      def to_latex
+        ""
+      end
+    end
+  end
+end
+
+
---
+++ test/test_latex.rb
@@ -0,0 +1,93 @@
+require 'test_helper'
+require 'hparser/block/all'
+require 'hparser/inline/all'
+require 'hparser/latex'
+
+class LatexTest < Test::Unit::TestCase
+  include HParser::Block
+  include HParser::Inline
+  def setup
+#    @parser = HParser::Parser.new
+  end
+
+  def assert_latex expect,node
+    assert_equal expect,node.to_latex
+  end
+
+  def test_blank
+    parser = HParser::Parser.new
+    assert_equal parser.parse(''), []
+  end
+
+  def test_head
+    assert_equal Head.head_level, 1
+    assert_latex "\\section{foo}\n\n",Head.new(1,[Text.new('foo')])
+    Head.head_level = 2
+    assert_latex "\\subsection{foo}\n\n",Head.new(1,[Text.new('foo')])
+    Head.head_level = 4
+    assert_latex "\\paragraph{foo}\n\n",Head.new(1,[Text.new('foo')])
+    Head.head_level = 1
+  end
+
+
+  def test_p
+    assert_latex "foobar\n\n",P.new([Text.new('foobar')])
+    assert_latex "\n\n",Empty.new
+  end
+
+  def test_pre
+    assert_latex "\\begin{verbatim}\nfoobar\n\\end{verbatim}\n",Pre.new([Text.new('foobar')])
+    assert_latex "\\begin{verbatim}\nfoobar\n\\end{verbatim}\n",SuperPre.new('foobar')
+  end
+
+  def test_quote
+    assert_latex "\\begin{quotation}\nfoobar\n\\end{quotation}\n",Quote.new([Text.new('foobar')])
+  end
+
+  def test_table
+    table = <<EOB
+\\begin{table}
+  \\centering
+  \\begin{tabular}{ l l }
+    foo & bar \\\\
+    baz & xyzzy \\\\
+  \\end{tabular}
+\\end{table}
+EOB
+    assert_latex table,
+                Table.new([th('foo'),th('bar')],
+                          [td('baz') ,td('xyzzy')])
+  end
+
+  def test_list
+    assert_latex "\\begin{itemize}\n  \\item aaa\n\n  \\item bbb\n\n\\end{itemize}\n",Ul.new(Li.new([Text.new('aaa')]),Li.new([Text.new('bbb')]))
+    assert_latex "\\begin{enumerate}\n  \\item aaa\n\n  \\item bbb\n\n\\end{enumerate}\n",Ol.new(Li.new([Text.new('aaa')]),Li.new([Text.new('bbb')]))
+#    assert_latex '<ol><ul><li>aaa</li></ul><li>bbb</li></ol>',Ol.new(Ul.new(Li.new([Text.new('aaa')])),
+#                                                                    Li.new([Text.new('bbb')]))
+  end
+
+  def test_dl
+    expect = <<EOB
+\\begin{description}
+\\item[foo] \\quad \\\\
+foo is ...
+\\item[bar] \\quad \\\\
+bar is ...
+
+\\end{description}
+EOB
+
+    first = Dl::Item.new([Text.new('foo')],[Text.new('foo is ...')])
+    second = Dl::Item.new([Text.new('bar')],[Text.new('bar is ...')])
+    assert_latex expect,Dl.new(first,second)
+  end
+
+  def th str
+    Th.new [Text.new(str)]
+  end
+
+  def td str
+    Td.new [Text.new(str)]
+  end
+
+end
