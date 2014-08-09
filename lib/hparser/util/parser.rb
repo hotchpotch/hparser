2535b31565cf0e4af5a57800dbbf69e153857374
nitoyon <nitoyon@gmail.com>
2014-08-10 08:31:42 +0900

    default_parser の列挙処理を HParser::Util::AllParser に移動
    HParser::Parser と HParser::Inline::Parser のクラスメソッドにあった
    default_parser の実装を HParser::Util::AllParser に移動した。

--- lib/hparser/util/parser.rb
+++ lib/hparser/util/parser.rb
@@ -69,6 +69,27 @@ module HParser
         @proc.call(*args)
       end
     end
+
+    class AllParser
+      # Retutrn array of all usable parser.
+      #
+      # This method collect all classes/modules which include
+      # mod module. And sorting those by <=>.
+      def self.includes(mod)
+        parser = []
+        ObjectSpace.each_object(Class){|klass|
+          if klass.include?(mod) then
+            parser.push klass
+          end
+        }
+
+        # sorting parser.
+        # e.g. Parser P should be after any other parser.
+        parser.sort{|a,b|
+          a <=> b or -(b <=> a).to_i
+        }
+      end
+    end
   end
 end
 
